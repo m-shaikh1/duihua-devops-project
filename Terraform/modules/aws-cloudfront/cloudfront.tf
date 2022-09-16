683430c155d2b761f0295862be6b48f1561eeff7
m-shaikh1 <mohammed.y.shaikh1@gmail.com>
2022-09-17 00:14:31 +0300

    Jenkinsfile

--- Terraform/modules/aws-cloudfront/cloudfront.tf
+++ Terraform/modules/aws-cloudfront/cloudfront.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.18.0"
+    }
+  }
+}
+
+provider "aws" {
+
+  region = "us-east-1"
+
+}
 locals {
   origin_id = "duihua-devops-project-cf" 
 }
