5ba561eb33d81666cb9c7b54fc54bdefa77483d7
m-shaikh1 <mohammed.y.shaikh1@gmail.com>
2022-09-17 17:32:56 +0300

    Jenkinsfile

--- Terraform/modules/aws-elasticbeanstalk-cloudfront/main.tf
+++ Terraform/modules/aws-elasticbeanstalk-cloudfront/main.tf
@@ -1,10 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.18.0"
+    }
+  }
+}
+
+provider "aws" {
+
+  region = "us-east-1"
+
+}
+resource "aws_elastic_beanstalk_application" "sudos-duihua-app" {
+  name        = var.eb-app-name
+}
+resource "aws_elastic_beanstalk_environment" "sudos-duihua-env" {
+  name                = var.eb-env-name
+  application         = aws_elastic_beanstalk_application.sudos-duihua-app.name
+  solution_stack_name = var.eb-solution-stack-name
+  
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+}
 locals {
   origin_id = "duihua-devops-project-cf" 
 }
 
 resource "aws_cloudfront_distribution" "distribution" {
   origin {
-    domain_name = "$(aws_elastic_beanstalk_environment.sudos-duihua-env.cname)"
+    domain_name = aws_elastic_beanstalk_environment.sudos-duihua-env.cname
     origin_id   = local.origin_id
 
     custom_origin_config {
