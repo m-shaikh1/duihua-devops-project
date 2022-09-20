c924fa12bb89fdccb43d5ee2da56442e7876aa90
m-shaikh1 <mohammed.y.shaikh1@gmail.com>
2022-09-20 12:27:20 +0300

    Added Jenkins Credentials to install.sh

--- Terraform/modules/aws-ec2/main.tf
+++ Terraform/modules/aws-ec2/main.tf
@@ -1,7 +1,11 @@
 resource "aws_instance" "ubuntu-ec2" {
   ami           = var.ami-image
   instance_type = var.instance_type
-  user_data = file("install.sh")
+  user_data = (templatefile("install.sh",{
+        AWS_ACCESS_KEY_ID      = var.AWS_ACCESS_KEY_ID
+        AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
+        AWS_REGION = var.AWS_REGION
+      }))
   associate_public_ip_address = true
   subnet_id = var.subnet_id 
   vpc_security_group_ids = [var.vpc_security_group_ids] 
